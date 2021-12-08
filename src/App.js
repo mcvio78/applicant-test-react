0d6f1731a7d90faac02587fe95000128e9efe320
Marek Mravík <marek.mravik@deso.cz>
2021-12-08 15:48:22 +0100

    Template update

--- src/App.js
+++ src/App.js
@@ -30,36 +30,42 @@ function App() {
             <div>Awesome book with black cover!</div>
             <div>299 CZK</div>
             <div>5+ in stock</div>
+            <input type="number" value={0}/>
             <button>Add to cart</button>
             <img src="./book-blue.jpg"/>
             <div>Blue book</div>
             <div>Literally awesomeness itself with blue cover.</div>
             <div>310 CZK</div>
             <div>3 in stock</div>
+            <input type="number" value={0}/>
             <button>Add to cart</button>
             <img src="./book-green.jpg"/>
             <div>Green book</div>
             <div>Great book with cover like grass.</div>
             <div>310 CZK</div>
             <div>Unavailable</div>
+            <input type="number" value={0}/>
             <button>Add to cart</button>
             <img src="./book-red.jpg"/>
             <div>Red book</div>
             <div>A book full of secrets.</div>
             <div>499 CZK</div>
             <div>1 in stock</div>
+            <input type="number" value={0}/>
             <button>Add to cart</button>
             <img src="./book-white.jpg"/>
             <div>White book</div>
             <div>A book with white cover - great for personal drawings!</div>
             <div>199 CZK</div>
             <div>5+ in stock</div>
+            <input type="number" value={0}/>
             <button>Add to cart</button>
             <img src="./book-green.jpg"/>
             <div>Yellow book</div>
             <div>Very sunny book for you!</div>
             <div>299 CZK</div>
             <div>Unavailable</div>
+            <input type="number" value={0}/>
             <button>Add to cart</button>
         </div>
     );
