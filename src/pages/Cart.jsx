42cf464be84fc8c577a17f7ac986d610390e6bdd
maurovio <mcvio78@gmail.com>
2022-04-02 02:24:37 +0200

    import CartBookRow and CartMainRow into the Cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,34 +1,10 @@
+import { CartMainRow } from "../components/CartMainRow";
+import { CartBookRow } from "../components/CartBookRow";
+
 export const Cart = () => (
   <div className="cart-list">
-    <div className="main-row">
-      <div className="name">Name</div>
-      <div className="amount">Amount</div>
-      <div className="price">Price</div>
-    </div>
-    <div className="book-row">
-      <div className="book-name">Black book</div>
-      <div className="amount">
-        2<button type="button">Remove</button>
-      </div>
-      <div className="price">598 CZK</div>
-    </div>
-    <div className="book-row">
-      <div className="book-name">Red book</div>
-      <div className="amount">
-        1<button type="button">Remove</button>
-      </div>
-      <div className="price">499 CZK</div>
-    </div>
-    <div className="book-row">
-      <div className="book-name">White book</div>
-      <div className="amount">
-        1<button type="button">Remove</button>
-      </div>
-      <div className="price">299 CZK</div>
-    </div>
-    <div className="main-row">
-      <div>Total</div>
-      <div>1396 CZK</div>
-    </div>
+    <CartMainRow firstSlot="Name" secondSlot="Amount" thirdSlot="Price" />
+    <CartBookRow bookName="Black book" selectedAmount={2} price={298} />
+    <CartMainRow firstSlot="Total" thirdSlot="1396 CZK" />
   </div>
 );
