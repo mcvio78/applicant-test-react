67c9c0ecb8ff0dbb327bf215360dd0eeace7589b
maurovio <mcvio78@gmail.com>
2022-04-02 19:40:12 +0200

    feeding Cart page with values from the context

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,14 +7,25 @@ import { CartContext } from "../state/context";
 export const Cart = () => {
   const {
     state: {
-      cart: { total },
+      cart: { total, selectedBooks },
+      books,
     },
+    removeTodoItem,
   } = useContext(CartContext);
 
   return (
     <div className="cart-list">
       <CartMainRow firstSlot="Name" secondSlot="Amount" thirdSlot="Price" />
-      <CartBookRow bookName="Black book" selectedAmount={2} price={298} />
+      {selectedBooks &&
+        Object.entries(selectedBooks).map(([id, selectedAmount]) => (
+          <CartBookRow
+            key={id}
+            bookName={books[id].name}
+            selectedAmount={selectedAmount}
+            price={books[id].price}
+            onClick={() => removeTodoItem(id)}
+          />
+        ))}
       <CartMainRow firstSlot="Total" thirdSlot={`${total} CZK`} />
     </div>
   );
